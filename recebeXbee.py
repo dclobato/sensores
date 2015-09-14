2f3475aedf52aa1c619f2880d558cc631056a67c
Daniel Correa Lobato <dclobato@users.noreply.github.com>
2015-09-14 18:01:21 -0300

    Create recebeXbee.py
    Exemplo de codigo para receber frames ZigBee (XBee serie 2) no python

---
+++ recebeXbee.py
@@ -0,0 +1,23 @@
+####
+## XBee: https://pypi.python.org/pypi/XBee/2.1.0
+##
+import serial, time, datetime, sys
+from xbee import XBee,ZigBee
+
+SERIALPORT = "/dev/ttyAMA0"    # the com/serial port the XBee is connected to, the pi GPIO should always be ttyAMA0
+BAUDRATE = 9600      # the baud rate we talk to the xbee
+
+ser = serial.Serial(SERIALPORT, BAUDRATE)
+
+xbee = ZigBee(ser,escaped=True)
+
+print 'Starting Up Tempature Monitor'
+# Continuously read and print packets
+while True:
+    try:
+        response = xbee.wait_read_frame()
+        print response
+    except KeyboardInterrupt:
+        break
+
+ser.close()
